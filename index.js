ff3197eb8826e545ff19bc75c229070727e9e216
tosih <sohail@runnable.com>
2016-11-16 11:28:02 -0800

    Adding initial files.

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Priest launcher script.
+ *
+ * @author Praful Rana <praful@runnable.com> & Sohail Ahmed <sohail@runnable.com>
+ */
+
+require('loadenv')()
+const priest = require('priest')
+priest.run()
